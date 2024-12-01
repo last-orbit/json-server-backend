2c05999be77f5821725b52a6e732a9384ea0a79a
Robert Ortiz <84008858+last-orbit@users.noreply.github.com>
2024-12-01 16:00:14 +0100

    Reverted back to previous commit

--- app.js
+++ app.js
@@ -1,37 +1,10 @@
-require('dotenv').config();
-
-const jsonServer = require('json-server');
-
-const morgan = require('morgan');
-
-//************************** */
-
-//new
-
-// create two variables, path and fs
-
-//path is a tool that has the .join method to join paths from the operating system to the another string
-
-const path = require('path');
-
-//fs is short for file system. Best metaphor is a librarian
-
-const fs = require('fs');
+require("dotenv").config();
+const jsonServer = require("json-server");
+const morgan = require("morgan");
 
 const server = jsonServer.create();
-
-//JSON parse 'parses' data into JS
-
-//fs.readFileSync is a method to read a file and return its content
-
-//__dirname is the absolute path from the computers root directory to the current location
-
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')));
-
-const router = jsonServer.router(db);
-
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
 const PORT = process.env.PORT || 5005;
 
 // const CORS = require('cors');
@@ -45,19 +18,14 @@ const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
   //   })
   // );
 server.use(middlewares);
-
-server.use(morgan('dev'));
-
+server.use(morgan("dev"));
 server.use((req, res, next) => {
   // Middleware to disable CORS
-
-  res.header('Access-Control-Allow-Origin', '*');
-
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
-
 server.use(router);
 
 server.listen(PORT, () => {
-  console.log(`JSON Server is running at port...${PORT}`);
+  console.log(`JSON Server is running at port ${PORT}`);
 });
